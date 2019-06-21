1292a9a567888771e88d4dc6399b36f4399baa41
tahamazari <tahamazari@gmail.com>
2019-06-21 16:29:14 +0500

    scss and babel

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import {log} from './App.js';
+import './index.scss';
+log()
---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+
+const webpack_rules = [];
+
+let babelLoader = {
+    test: /\.js$/,
+    exclude: /(node_modules|bower_components)/,
+    use: {
+        loader: "babel-loader",
+        options: {
+            presets: ["@babel/preset-env"]
+        }
+    }
+};
+
+webpack_rules.push(babelLoader);
+
+module.exports = {
+    devtool: "none",
+    entry: "./src/index.js",
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/template.html"
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    }
+}
